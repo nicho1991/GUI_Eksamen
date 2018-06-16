10fdbd37254f3ece4b59d282254769a33fc4d7b7
nicho1991 <ladefoged1991@gmail.com>
2018-06-16 21:21:57 +0200

    start of 11

---
+++ Nicholas_Gennemgang af lab/11 Windows and dialogs/Agents/Converters.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Agents
+{
+    public class ColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string AgentID = value.ToString();
+            if (value != null && value == "007")
+            {
+                return new SolidColorBrush(Colors.Red);
+            }
+
+            return new SolidColorBrush(Colors.Blue);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Nicholas_Gennemgang af lab/11 Windows and dialogs/Agents/Specialities.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agents
+{
+    public class Specialities : ObservableCollection<string>, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void
+            Notify([CallerMemberName]string propName = null
+            )
+        {
+            var handler
+                = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this
+                    , new PropertyChangedEventArgs(propName));
+            }
+        }
+
+        public Specialities()
+        {
+            Add("Coding");
+            Add("Arkitekture");
+            Add("Design");
+            Add("Lazy");
+        }
+
+    }
+
+    
+}
